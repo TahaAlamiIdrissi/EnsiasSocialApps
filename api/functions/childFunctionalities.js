ad0eb97bd77972e60c7f62ce21bcd48ab1ea0ada
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-03-24 19:32:55 +0100

    added the getchild by id functionality

--- api/functions/childFunctionalities.js
+++ api/functions/childFunctionalities.js
@@ -1,15 +1,13 @@
 const mysqlConnect = require('../../db');
 
 
-
-
 function getAllChilds(){
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
         // The Promise constructor should catch any errors thrown on
         // this tick. Alternately, try/catch and reject(err) on catch.
 
         const query_str =`SELECT * FROM children`;
-        mysqlConnect.query(query_str, function (err, rows, fields) {
+        mysqlConnect.query(query_str,(err, rows, fields) => {
             // Call reject on error states,
             // call resolve with results
             if (err) {
@@ -19,3 +17,23 @@ function getAllChilds(){
         });
     });
 }
+
+function getChildById(id){
+    return new Promise((resolve,reject) => {
+        const query_str =`SELECT * FROM children WHERE id = ? `;
+        const query_param = [id];
+
+        mysqlConnect.query(query_str,query_param,(err,rows,fields) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(rows);
+        });
+
+    });
+}
+
+module.exports = {
+    getAllChilds ,
+    getChildById
+};
