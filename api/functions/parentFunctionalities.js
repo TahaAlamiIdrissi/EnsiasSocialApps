b05cde2732c85bb5a5254275b6c5e29dd184cc07
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-03-30 19:15:07 +0100

    added the get all parents functionality

--- api/functions/parentFunctionalities.js
+++ api/functions/parentFunctionalities.js
@@ -1,3 +1,20 @@
 const mysqlConnect = require('../../db');
 
 
+function getAllParents(){
+    return new Promise((resolve,reject) => {
+        const query_str=`SELECT * 
+                         FROM users 
+                         WHERE type like 'parent' `;
+        mysqlConnect.query(query_str,(err,rows,fields) =>{
+            if(err)
+                return reject(err);
+            
+            resolve(rows);
+        })
+    });
+}
+
+module.exports = {
+    getAllParents
+}
