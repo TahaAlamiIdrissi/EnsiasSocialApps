75deb751c2bfe6426424b894d1ea10ec60e941e9
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-03-30 18:55:32 +0100

    added the parent functionalities file and the route file

---
+++ api/functions/parentFunctionalities.js
@@ -0,0 +1,3 @@
+const mysqlConnect = require('../../db');
+
+
---
+++ api/routes/parent.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const parentFunc = require('../functions/parentFunctionalities');
+
+
+
+
+
+
+module.exports = router;
