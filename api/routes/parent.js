93334827883263352b7219223a9b222ab41339a7
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-03-30 19:29:50 +0100

    implemented the get all parent route

--- api/routes/parent.js
+++ api/routes/parent.js
@@ -2,7 +2,30 @@ const express = require('express');
 const router = express.Router();
 const parentFunc = require('../functions/parentFunctionalities');
 
-
+router.get('/',(req,res,next) => {
+    parentFunc.getAllParents()
+              .then(docs => {
+                  const response = {
+                      count: docs.length,
+                      parents: docs.map(doc => {
+                          return {
+                              doc,
+                              request: {
+                                  type: 'GET',
+                                  url: `http://localhost:3000/parents/${doc.id}`
+                              }
+                          }
+                      })
+                  }
+                  res.status(200)
+                     .json(response);
+              })
+              .catch(err => {
+                  console.log(err);
+                  res.status(500)
+                     .json(err);
+              });
+});
 
 
 
