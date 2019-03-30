93411be2cef9b1a9e7eeae75c0143357824e6ca6
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-03-30 19:37:10 +0100

    added the get parent by id functionality

--- api/functions/parentFunctionalities.js
+++ api/functions/parentFunctionalities.js
@@ -3,7 +3,7 @@ const mysqlConnect = require('../../db');
 
 function getAllParents(){
     return new Promise((resolve,reject) => {
-        const query_str=`SELECT * 
+        const query_str = `SELECT * 
                          FROM users 
                          WHERE type like 'parent' `;
         mysqlConnect.query(query_str,(err,rows,fields) =>{
@@ -11,10 +11,26 @@ function getAllParents(){
                 return reject(err);
             
             resolve(rows);
-        })
+        });
+    });
+}
+
+function getParentById(id){
+    return new Promise((reslove,reject) => {
+        const query_str = `SELECT * 
+                           FROM users
+                           WHERE type like 'parent' AND id = ?`;
+        const query_param = [id];
+        mysqlConnect.query(query_str,query_param,(err,rows,fields) => {
+            if(err)
+                return reject(err);
+            
+            resolve(rows);
+        });
     });
 }
 
 module.exports = {
-    getAllParents
+    getAllParents,
+    getParentById
 }
