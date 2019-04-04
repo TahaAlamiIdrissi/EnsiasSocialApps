e6fd195bb1976721aa67ac59227ba4cbf7f93afb
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-04-04 19:18:10 +0100

    Added the get Tutor by id functionality

--- api/functions/tutorFunctionalities.js
+++ api/functions/tutorFunctionalities.js
@@ -15,9 +15,24 @@ function getAllTutors(){
 }
 
 
+function getTutorById(id){
+    return new Promise((resolve,reject) => {
+        const query_str = `SELECT *
+                           FROM users
+                           WHERE type like 'tuteur' AND id=?`;
+        const query_param = [id];
 
+        mysqlConnect.query(query_str,query_param,(err,rows,fields) => {
+            if(err)
+                return reject(err);
+                
+            resolve(rows);
+        })
+    });
+}
 
 
 module.exports = {
-    getAllTutors
+    getAllTutors,
+    getTutorById
 }
